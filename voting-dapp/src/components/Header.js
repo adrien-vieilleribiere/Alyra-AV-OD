4cf5d39d4860e9aedf7a9eefb4786b55c6771581
0livierDAVID <o.david@yahoo.fr>
2023-03-03 09:41:35 +0100

    Separation of the AppBar and the stepper progress indicator

--- voting-dapp/src/components/Header.js
+++ voting-dapp/src/components/Header.js
@@ -14,35 +14,39 @@ import {
 
 function Header({ step, steps }) {
   return (
-    <Box
-      component="header"
-      sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}
-      className="App-header"
-    >
-      <Box sx={{ flexGrow: 1, mt:1, mb:4 }}>
+    <>
+      <Box sx={{ flexGrow: 1, mb: 2 }}>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h5" component="h1" sx={{ flexGrow: 1 }}>
               Voting app
             </Typography>
-             {/* TODO: Add Metamask connection widget*/}
+            {/* TODO: Add Metamask connection widget*/}
             <Button color="inherit">Metamask widget</Button>
           </Toolbar>
         </AppBar>
       </Box>
 
-      {/* Stepper */}
-      <Box sx={{ width: '100%' }}>
-        <Stepper activeStep={step} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
+      <Box
+        component="header"
+        sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}
+        className="App-header"
+      >
 
-    </Box>
+
+        {/* Stepper */}
+        <Box sx={{ width: '100%' }}>
+          <Stepper activeStep={step} alternativeLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+
+      </Box>
+    </>
   );
 };
 
