83eea94bc0ea07aa2ab91e5b64fc776f91bd2dc5
adrien-v <adrien.v@gmail.com>
2023-03-04 15:50:40 +0100

    refix test

--- voting-dapp/src/components/App.test.js
+++ voting-dapp/src/components/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Voting/i);
+  const linkElement = screen.getByText(/Voting app/i);
   expect(linkElement).toBeInTheDocument();
 });
