81d300a8a8a49486589a3f538e150ece98d48c22
OlivierDAVID <8449210+0livierDAVID@users.noreply.github.com>
2023-03-08 16:19:42 +0100

    Responsive aspect improvement (#14)
    * Cleanup of example files
    
    * App skeleton implemented with all the major components
    
    * Change default step value to 0
    
    * Improvment of small resolutions management
    
    * Quickfix on Aside component badge position

--- client/src/components/Header/HeaderStepper.jsx
+++ client/src/components/Header/HeaderStepper.jsx
@@ -1,33 +1,77 @@
 import {
   Box,
+  Paper,
+  Typography,
+  MobileStepper,
   Stepper,
   Step,
   StepLabel,
 } from '@mui/material';
 
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 import useEth from "../../contexts/EthContext/useEth";
 import { stepsLabel } from '../../helper/const';
 
 function HeaderStepper() {
+  const theme = useTheme();
+  const mobile = !useMediaQuery(theme.breakpoints.up('sm'));
+
   const { state: { currentStep } } = useEth();
 
   return (
-    <Box
-      component="header"
-      sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}
-      className="App-header"
-    >
-      {/* Stepper */}
-      <Box sx={{ width: '100%' }}>
-        <Stepper activeStep={currentStep} alternativeLabel>
-          {stepsLabel.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
+    <>
+    {/* Mobile display */}
+      {mobile && (
+        <Box
+          component="header"
+          sx={{ 
+            p: 2, border: '1px solid grey', borderRadius: '10px',
+            maxWidth: 600, flexGrow: 1, display: 'flex', flexDirection: 'column',
+            alignItems: 'center'
+          }}
+          className="App-header"
+        >
+          <Typography sx={{ color: 'black', textAlign: 'center' }}>
+            {stepsLabel[currentStep]}
+          </Typography>
+          <MobileStepper
+            variant="dots"
+            steps={stepsLabel.length}
+            position="static"
+            activeStep={currentStep}
+            sx={{ backgroundColor: 'var(--light-grey)' }}
+          />
+          <MobileStepper
+            variant="text"
+            steps={stepsLabel.length}
+            position="static"
+            activeStep={currentStep}
+            sx={{ backgroundColor: 'var(--light-grey)'}}
+          />
+        </Box>
+      )}
+      {/* Desktop display */}
+      {!mobile && (
+      <Box
+        component="header"
+        sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}
+        className="App-header"
+      >
+        {/* Stepper */}
+        <Box sx={{ width: '100%' }}>
+          <Stepper activeStep={currentStep} alternativeLabel>
+            {stepsLabel.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
       </Box>
-    </Box>
+      )}
+    </>
   );
 };
 
