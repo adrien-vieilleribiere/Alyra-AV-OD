d29c724a07460716cf0b97ada35a6eb3fda1d1be
0livierDAVID <o.david@yahoo.fr>
2023-03-08 08:26:04 +0100

    Cleanup of example files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,12 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
 import Footer from "./components/Footer";
-import Examples from "./components/Demo/Examples";
 
 function App() {
   return (
     <EthProvider>
       <div id="App">
         <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Examples />
-          <Demo />
-          <hr />
+
           <Footer />
         </div>
       </div>
