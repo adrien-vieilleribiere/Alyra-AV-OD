17dd0e4ac9987d19553104fe29c39b83a9f85b91
0livierDAVID <o.david@yahoo.fr>
2023-03-08 10:57:51 +0100

    App skeleton implemented with all the major components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,32 @@
+import {
+  Box,
+  Divider
+} from '@mui/material';
+
 function Link({ uri, text }) {
-  return <a href={uri} target="_blank" rel="noreferrer">{text}</a>;
+  return <a
+    href={uri}
+    className="App-link"
+    target="_blank"
+    rel="noopener noreferrer">{text}
+  </a>;
 }
 
 function Footer() {
   return (
-    <footer>
-
-    </footer >
+    <Box
+      component="footer"
+      mt={2}
+      sx={{ p: 2, border: '1px solid grey', borderRadius: '10px' }}
+      className="App-footer"
+    >
+      <div>Made with &#10084; for Alyra</div>
+      <Divider sx={{ my: 2 }} />
+      <Link
+        uri={"https://github.com/adrien-vieilleribiere/Alyra-AV-OD"}
+        text={"Github sources"}
+      />
+    </Box>
   );
 }
 
---
+++ client/src/helper/const.js
@@ -0,0 +1,14 @@
+export const stepsLabel = [
+    'Registering Voters',
+    'Proposals Registration Started',
+    'Proposals Registration Ended',
+    'Voting Session Started',
+    'Voting Session Ended',
+    'VotesTallied'
+]
+
+export const roles = {
+    NONE: 0,
+    VOTER: 1,
+    OWNER: 2,
+};
