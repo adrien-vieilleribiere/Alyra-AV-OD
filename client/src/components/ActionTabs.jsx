e291cafc28b7c7023fa4f3946ca556783d0f8b61
adrien-v <adrien.v@gmail.com>
2023-03-13 22:50:01 +0100

    Add a SC call into the AddVoter component

--- client/src/components/ActionTabs.jsx
+++ client/src/components/ActionTabs.jsx
@@ -22,11 +22,12 @@ import GetVoteTab from "../tabs/GetVoteTab";
 import GetWinnerTab from "../tabs/GetWinnerTab";
 
 function ActionTabs() {
-  const { state: { step, user : { /*isConnected,*/ isOwner, isVoter } } } = useEth();
+  const { state: { step, user: { /*isConnected,*/ isOwner, isVoter } } } = useEth();
 
   const [currentTab, SetCurrentTab] = useState("info");
-  
+
   const handleChange = (evt, val) => {
+    SetCurrentTab("info");
     SetCurrentTab(val);
   }
 
