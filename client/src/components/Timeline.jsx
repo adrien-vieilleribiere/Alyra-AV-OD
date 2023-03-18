d1a234faf61559d23be4da8fc1d5e4bdeab68c4b
OlivierDAVID <o.david@yahoo.fr>
2023-03-18 18:47:53 +0100

    conditionnal display of the timeline

--- client/src/components/Timeline.jsx
+++ client/src/components/Timeline.jsx
@@ -2,21 +2,27 @@ import {
   Box,
 } from '@mui/material';
 
+import useEth from "./../contexts/EthContext/useEth";
+
 import StepsTimeline from './StepsTimeline';
 import StepIncrementer from './StepIncrementer';
 
 function Timeline() {
+  const { state: { user: { isConnected } } } = useEth();
+
   return (
     <>
-      <Box
-        sx={{ p: 2, border: '1px solid grey', borderRadius: '10px', background: 'var(--mid-gray)' }}
-      >
-        <StepsTimeline />
-        <Box textAlign='center' sx={{ flexGrow: 1, marginTop: 1 }}>
-          {/* <StepIncrementer step={step} /> */}
-          <StepIncrementer />
+      {isConnected &&
+        <Box
+          sx={{ p: 2, border: '1px solid grey', borderRadius: '10px', background: 'var(--mid-gray)' }}
+        >
+          <StepsTimeline />
+          <Box textAlign='center' sx={{ flexGrow: 1, marginTop: 1 }}>
+            {/* <StepIncrementer step={step} /> */}
+            <StepIncrementer />
+          </Box>
         </Box>
-      </Box>
+      }
     </>
   )
 
