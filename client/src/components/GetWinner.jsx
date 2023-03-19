e3fecd568d8fc056fff6611d2371900fcfcd34f5
adrien-v <adrien.v@gmail.com>
2023-03-19 11:42:33 +0100

    get Winner

--- client/src/components/GetWinner.jsx
+++ client/src/components/GetWinner.jsx
@@ -1,6 +1,30 @@
+import React, { useEffect, useState } from "react";
+import useEth from "../contexts/EthContext/useEth";
+import { Box } from '@mui/material';
 function GetWinner() {
+  let { state: { contract, accounts, proposals } } = useEth();
+  const [winner, setWinner] = useState(0);
+  useEffect(() => {
+    (async function () {
+      console.log("PROPS STATR", proposals);
+      const getWinningProp = parseInt(await contract.methods.winningProposalID().call({ from: accounts[0] }));
+      console.log("WIINNING3", getWinningProp);
+      if (getWinningProp > 0) {
+        const bestProposalDescription = proposals.filter(prop => parseInt(prop.id) === getWinningProp);
+        console.log("bp", bestProposalDescription);
+        if (bestProposalDescription.length > 0) {
+          setWinner(bestProposalDescription[0].description);
+        }
+
+      }
+    })();
+  },);
+
   return (
-    <p>TODO</p>
+    <Box sx={{ minHeigth: "3rem" }}>
+      <p>And the winner proposal is: {winner} !!!</p>
+      <div class="firework"></div>
+    </Box>
   );
 }
 export default GetWinner;
