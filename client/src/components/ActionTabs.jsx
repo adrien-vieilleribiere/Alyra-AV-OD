8a922c0e9bd310d309705cfb8630ba0fc92e841d
OlivierDAVID <o.david@yahoo.fr>
2023-03-20 10:23:15 +0100

    test tab change method removed

--- client/src/components/ActionTabs.jsx
+++ client/src/components/ActionTabs.jsx
@@ -29,8 +29,7 @@ import GetVoteTab from "./tabs/GetVoteTab";
 import GetWinnerTab from "./tabs/GetWinnerTab";
 
 function ActionTabs() {
-  const { state: { /*step,*/ user: { isConnected, isOwner, isVoter } } } = useEth();
-  const step = 3;
+  const { state: { step, user: { isConnected, isOwner, isVoter } } } = useEth();
   const [currentTab, SetCurrentTab] = useState("info");
 
   const handleChange = (evt, val) => {
